b4ae4d4fc3f9a7effc3060402f4c8e3a0a37691f
Corey Farwell <coreyf@rwell.org>
2011-10-27 20:31:34 +0000

    updated files to work with IE7/8, new onload and js format

--- viewer/hyperlinks.js
+++ viewer/hyperlinks.js
@@ -1,10 +1,6 @@
-var latlngSV;
-
-function init(lat, lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    latlngSV = new google.maps.LatLng(0,0);
-
-    //----------------------------------//
+window.onload = (function () {
+    var latlngSV = new google.maps.LatLng(0,0),
+        latlng = new google.maps.LatLng(lat, lng);
 
     var mapOptions = {
         center: latlng,
@@ -13,33 +9,26 @@ function init(lat, lng) {
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-    //----------------------------------//
-
     var sv = new google.maps.StreetViewService();
     sv.getPanoramaByLocation(latlng, 50, processSVData);
     var heading = google.maps.geometry.spherical.computeHeading(latlngSV, latlng);
 
-    //----------------------------------//
-
-    var panoramaOptions = {
+    var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"), {
         position: latlng,
         pov: {
             heading: heading,
             pitch: 10,
-            zoom: 0,
+            zoom: 0
         }
-    };
-    var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"), panoramaOptions);
-
-    //----------------------------------//
+    });
 
     map.setStreetView(panorama);
-}
 
-function processSVData(data, status) {
-    if (status == google.maps.StreetViewStatus.OK) {
-        latlngSV = data.location.latLng;
-    } else {
-        alert("Street View data not found for this location.");
+    function processSVData(data, status) {
+        if (status == google.maps.StreetViewStatus.OK) {
+            latlngSV = data.location.latLng;
+        } else {
+            alert("Street View data not found for this location.");
+        }
     }
-}
+});
--- viewer/streetview.cgi
+++ viewer/streetview.cgi
@@ -4,10 +4,10 @@ import cgi
 from mako.template import Template
 
 form = cgi.FieldStorage()
-lng = form["lng"].value
-lat = form["lat"].value
+lng = form.getvalue("lng", 0)
+lat = form.getvalue("lat", 0)
 
-print("Content-type: text/html\n")
+print("Content-type: text/html\n\n")
 
 mytemplate = Template(filename='streetview.mak.html')
 print(mytemplate.render(latitude=lat,longitude=lng))
